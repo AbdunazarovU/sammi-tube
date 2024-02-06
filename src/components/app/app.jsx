91f73da6b48299810e066174327f77b9cfb3813e
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-07 00:41:25 +0500

    add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,19 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useParams } from "react-router-dom"
 import { Box } from "@mui/material"
-import { Channel, Main } from "../"
+import { Channel, Main, Navbar, Search, VideoDetail } from "../"
 
 const App = () => {
+  const params = useParams()
+  console.log(params)
+
   return (
     <Box>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/channel" element={<Channel />} />
+        <Route path="/channel/:id" element={<Channel />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/search/:id" element={<Search />} />
       </Routes>
     </Box>
   )
