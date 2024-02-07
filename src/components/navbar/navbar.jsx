d0f9ed5adc175284bfe6c0ecb964502b62421e87
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-07 11:38:00 +0500

    add navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,25 @@
+import { Stack, Box } from "@mui/material";
+import { logo } from "../../constants";
+import { colors } from "../../constants/colors";
+import { Link } from "react-router-dom";
+import { SearchBar } from "../";
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <Stack
+      direction={"row"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+      p={2}
+      sx={{position: "sticky", top: 0, zIndex: 99, backgroundColor: colors.primary }}
+    >
+      <Link to={"/"}>
+        <img src={logo} alt="logo rasmi" width={120} height={40} />
+      </Link>
+      <SearchBar />
+      <Box />
+    </Stack>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const logo = "https://picsum.photos/200/300?random=1"
