44f519ee41e94a95d9392e1538e4c6702601e5b7
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-08 11:16:49 +0500

    add search page with styles

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,16 +1,33 @@
-import { Search } from "@mui/icons-material"
-import { Paper, IconButton } from "@mui/material"
-import { colors } from "../../constants/colors"
+import { Search } from "@mui/icons-material";
+import { Paper, IconButton } from "@mui/material";
+import { colors } from "../../constants/colors";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
 
 const SearchBar = () => {
+  const [value, setValue] = useState("")
+  const navigate = useNavigate()
+
+  const submitHandler = (evt) => {
+    evt.preventDefault()
+    if (value) {
+      navigate(`/search/${value}`)
+    }
+    setValue("")
+  }
+
   return (
-    <Paper component={"form"} sx={{ border: `1px solid ${colors.secondary}`, pl: 2, boxShadow: "none" }} >
-      <input className="search-bar" type="text" placeholder="Search..." />
-      <IconButton>
+    <Paper
+      component={"form"}
+      onSubmit={submitHandler}
+      sx={{ border: `1px solid ${colors.secondary}`, pl: 2, boxShadow: "none", mr: 5, }}
+    >
+      <input className="search-bar" type="text" placeholder="Search..." onChange={e => setValue(e.target.value)} value={value} />
+      <IconButton type="submit" >
         <Search />
       </IconButton>
     </Paper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
